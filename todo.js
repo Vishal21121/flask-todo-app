786cb7c66abc24a37edfde4417921c8466b3230c
Vishal21121 <vs84077000@gmail.com>
2023-05-11 19:11:29 +0530

    added: update button functionality

--- todo.js
+++ todo.js
@@ -1,19 +1,19 @@
-(function(){
-    
+(function () {
+
     insertTodos()
 
-    document.getElementById('submit').addEventListener("click", (e)=>{
+    document.getElementById('submit').addEventListener("click", (e) => {
         let val = document.getElementById("input").value
-        if(val.length>0){
+        if (val.length > 0) {
             addTodo(val)
         }
     })
-    
-    document.getElementById('input').addEventListener("keyup", (event)=>{
+
+    document.getElementById('input').addEventListener("keyup", (event) => {
         let val = document.getElementById("input").value
         if (event.key === 'Enter') {
             console.log(val);
-            if(val.length>0){
+            if (val.length > 0) {
                 addTodo(val)
             }
         }
@@ -22,57 +22,86 @@
 })()
 
 // ! TODO
-async function deleteTodo(id) {
-
-}
-
-function addListener(){
-   let editElements = document.getElementsByClassName("delete")
-//    console.log(editElements);
-   Array.from(editElements).forEach(element => {
-        element.addEventListener("click",async (element)=>{
+async function deleteTodo(deleatable) {
+    Array.from(deleatable).forEach(element => {
+        element.addEventListener("click", async (element) => {
             const id = element.target.parentNode.firstElementChild.firstElementChild.id
-            const response = await fetch(`http://localhost:8081/deleteTodo/${id}`)
+            const response = await fetch(`http://localhost:8081/deleteTodo/${id}`, { method: "DELETE" })
             const data = await response.json()
             console.log(data);
-            if(data["status"]=="success"){
+            if (data["status"] == "success") {
                 console.log(data["message"]);
                 insertTodos()
             }
         })
-   });
+    });
 }
 
-async function todoFetch(){
+async function updateTodo(editable) {
+    Array.from(editable).forEach((element) => {
+        element.addEventListener("click", (event) => {
+            const buttonEl = document.createElement("button");
+            buttonEl.classList.add("text-gray-400", "outline", "outline-orange-700", "rounded-xl", "py-0.5", "font-semibold", "px-4", "hover:bg-orange-600", "hover:text-white", "mt-1", "mr-4", "delete");
+            buttonEl.id = "save"
+            buttonEl.textContent = "SAVE";
+            event.target.parentNode.appendChild(buttonEl);
+            let contentElement = event.target.parentNode.firstElementChild.firstElementChild
+            contentElement.setAttribute("contenteditable","true")
+            document.getElementById('save').addEventListener("click",async ()=>{
+                let val = contentElement.innerText
+                let todoId = contentElement.id
+                const response = await fetch(`http://localhost:8081/updateTodo/${todoId}`,{
+                    method:'cors',
+                    method:"PATCH",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify({"title":val})
+                })
+                insertTodos()
+            })
+
+        })
+    })
+}
+
+function addListener() {
+    let deleatable = document.getElementsByClassName("delete")
+    deleteTodo(deleatable)
+    let editable = document.getElementsByClassName("edit")
+    updateTodo(editable)
+    
+}
+
+async function todoFetch() {
     let userid = localStorage.getItem("userid")
     const response = await fetch(`http://localhost:8081/getTodo/${userid}`)
     return response
 }
 
-async function addTodo(val){
+async function addTodo(val) {
     let userid = localStorage.getItem("userid")
-    let response = await fetch(`http://localhost:8081/addTodo/${userid}`,{
-        mode:'cors',
-        method:'POST',
+    let response = await fetch(`http://localhost:8081/addTodo/${userid}`, {
+        mode: 'cors',
+        method: 'POST',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"title":val})
+        body: JSON.stringify({ "title": val })
     })
     let data = await response.json()
-    let id =  data["data"]
+    let id = data["data"]
     console.log(id);
-    insertTodoId(val,id)
+    insertTodoId(val, id)
 }
 
-async function insertTodoId(title,id){
+async function insertTodoId(title, id) {
     document.getElementById("todoContainer").innerHTML += `
     <div
                 class="w-1/2 bg-gray-900 ml-40 p-2 my-4 rounded-3xl flex justify-center focus:border-5 border-5 border-red-800 ">
                 <div class="w-full">
-                    <input type="text"
-                        class="bg-gray-900  w-11/12 text-gray-400 h-8 border-none focus:outline-none pl-4"
-                        placeholder="What's your next task?" value="${title}" id="${id}">
+                    <p class="bg-gray-900  w-11/12 text-gray-400 h-8 border-none focus:outline-none pl-4 align-middle"
+                        placeholder="What's your next task?" id="${id}">${title}</p>
                 </div>
                 <button
                     class="text-gray-400 outline outline-blue-700 rounded-xl py-0.5 font-semibold px-4  hover:bg-blue-700 hover:text-white mt-1 mr-4 edit">EDIT
@@ -85,18 +114,16 @@ async function insertTodoId(title,id){
     document.getElementById("input").value = ''
 }
 
-async function insertTodos(){
+async function insertTodos() {
     let response = await todoFetch()
     let data = await response.json()
     document.getElementById("todoContainer").innerHTML = ''
-    data.data.forEach(({title,todoId}) => {
+    data.data.forEach(({ title, todoId }) => {
         document.getElementById("todoContainer").innerHTML += `
-    <div
+            <div
                 class="w-1/2 bg-gray-900 ml-40 p-2 my-4 rounded-3xl flex justify-center focus:border-5 border-5 border-red-800 ">
                 <div class="w-full">
-                    <input type="text"
-                        class="bg-gray-900  w-11/12 text-gray-400 h-8 border-none focus:outline-none pl-4"
-                        placeholder="What's your next task?" value="${title}" id="${todoId}">
+                    <p class="bg-gray-900  w-11/12 text-gray-400 h-8 border-none focus:outline-none pl-4 align-middle" id="${todoId}">${title}</p>
                 </div>
                 <button
                     class="text-gray-400 outline outline-blue-700 rounded-xl py-0.5 font-semibold px-4  hover:bg-blue-700 hover:text-white mt-1 mr-4 edit">EDIT
@@ -104,7 +131,7 @@ async function insertTodos(){
                 <button
                     class="text-gray-400 outline outline-red-700 rounded-xl py-0.5 font-semibold px-4  hover:bg-red-700 hover:text-white mt-1 mr-4 delete" >DELETE
                 </button>
-    </div>`
+            </div>`
     });
     addListener()
     document.getElementById("input").value = ''
